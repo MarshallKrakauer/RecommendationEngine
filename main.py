a3493875c9c9e2baf99f9dd5b6b94d94784f2ad8
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-12-26 18:16:57 -0800

    fixing files

--- main.py
+++ main.py
@@ -3,6 +3,4 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # import tensorflow as tf
-    # import tensorflow_recommenders as tfrs
     import pandas as pd
