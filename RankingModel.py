312fa31300802df0df3aaa916df6dc2e172eac80
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-12-27 18:28:06 -0800

    rank single game

--- RankingModel.py
+++ RankingModel.py
@@ -6,6 +6,7 @@ Based on the code from this page: https://www.tensorflow.org/recommenders/exampl
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import copy
 
 
 class RankingModel(tf.keras.Model):
@@ -52,12 +53,16 @@ class RankingModel(tf.keras.Model):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     tf.autograph.set_verbosity(0)
+    tf.random.set_seed(0)
 
     ratings = pd.read_csv('DataFiles/ratings_data_0.csv').append(pd.read_csv('DataFiles/ratings_data_1.csv'))
     ratings = ratings[['ratings', 'user_idx', 'game_idx']]
-    tf.random.set_seed(0)
-    example = {name: values for name, values in ratings.items()}
-    ratings = tf.data.Dataset.from_tensor_slices(example)
+    original_ratings = copy.deepcopy(ratings)
+    value_dict = {name: values for name, values in ratings.items()}
+    value_dict['ratings'] = np.asarray(value_dict['ratings'].astype('float32'))
+    value_dict['user_idx'] = np.asarray(value_dict['user_idx'].astype('string'))
+    value_dict['game_idx'] = np.asarray(value_dict['game_idx'].astype('string'))
+    ratings = tf.data.Dataset.from_tensor_slices(value_dict)
     shuffled = ratings.shuffle(400_000, seed=0, reshuffle_each_iteration=False)
     train = shuffled.take(80_000)
     test = shuffled.skip(80_000).take(20_000)
@@ -66,7 +71,5 @@ if __name__ == '__main__':
 
     unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
     unique_user_ids = np.unique(np.concatenate(list(user_ids)))
-    model = RankingModel()
-    temp = model(RankingModel()((["1"], ["1"])))
-    print(temp)
-    print("run is finished")
+    print(RankingModel()((['0'], ['0'])))
+
