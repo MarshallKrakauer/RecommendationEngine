88f5e45052e8f8bf6151a854706b81023b2470b7
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-12-27 17:39:07 -0800

    begin ranking file

---
+++ RankingModel.py
@@ -0,0 +1,72 @@
+"""Ranking Model using TF Version 2.
+
+Based on the code from this page: https://www.tensorflow.org/recommenders/examples/basic_ranking
+"""
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+class RankingModel(tf.keras.Model):
+
+    def get_config(self):
+        pass
+
+    def __init__(self):
+        super().__init__()
+        embedding_dimension = 32
+
+        # Compute embeddings for users.
+        self.user_embeddings = tf.keras.Sequential([
+            tf.keras.layers.StringLookup(
+                vocabulary=unique_user_ids, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+        ])
+
+        # Compute embeddings for movies.
+        self.movie_embeddings = tf.keras.Sequential([
+            tf.keras.layers.StringLookup(
+                vocabulary=unique_movie_titles, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding_dimension)
+        ])
+
+        # Compute predictions.
+        self.ratings = tf.keras.Sequential([
+            # Learn multiple dense layers.
+            tf.keras.layers.Dense(256, activation="relu"),
+            tf.keras.layers.Dense(64, activation="relu"),
+            # Make rating predictions in the final layer.
+            tf.keras.layers.Dense(1)
+        ])
+
+    def call(self, inputs, **kwargs):
+        user_id, movie_title = inputs
+
+        user_embedding = self.user_embeddings(user_id)
+        movie_embedding = self.movie_embeddings(movie_title)
+
+        return self.ratings(tf.concat([user_embedding, movie_embedding], axis=1))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tf.autograph.set_verbosity(0)
+
+    ratings = pd.read_csv('DataFiles/ratings_data_0.csv').append(pd.read_csv('DataFiles/ratings_data_1.csv'))
+    ratings = ratings[['ratings', 'user_idx', 'game_idx']]
+    tf.random.set_seed(0)
+    example = {name: values for name, values in ratings.items()}
+    ratings = tf.data.Dataset.from_tensor_slices(example)
+    shuffled = ratings.shuffle(400_000, seed=0, reshuffle_each_iteration=False)
+    train = shuffled.take(80_000)
+    test = shuffled.skip(80_000).take(20_000)
+    movie_titles = ratings.batch(1_000_000).map(lambda x: x["game_idx"])
+    user_ids = ratings.batch(1_000_000).map(lambda x: x["user_idx"])
+
+    unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
+    unique_user_ids = np.unique(np.concatenate(list(user_ids)))
+    model = RankingModel()
+    temp = model(RankingModel()((["1"], ["1"])))
+    print(temp)
+    print("run is finished")
